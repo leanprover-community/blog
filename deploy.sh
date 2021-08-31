5f167b8e538cc0d5f1c35857e2a26a5b184814f2
Rob Lewis <rob.y.lewis@gmail.com>
2021-08-31 17:11:26 -0400

    attempt at CI

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+set -e
+set -x
+
+# is $DEPLOY_GITHUB_USER: needed?
+git clone --branch deploy "https://$DEPLOY_GITHUB_TOKEN@github.com/leanprover-community/blog.git" ./build
+
+nikola build
+rm -rf ../build/docs
+mv output ../build/docs
+
+if [ "$github_repo" = "leanprover-community/blog" -a "$github_ref" = "refs/heads/master" ]; then
+  cd build/
+  git config user.email "leanprover.community@gmail.com"
+  git config user.name "leanprover-community-bot"
+  git add -A .
+  git diff-index HEAD
+  git diff-index --quiet HEAD || { git commit -m "deploy site from $git_hash" && git push; }
+fi
